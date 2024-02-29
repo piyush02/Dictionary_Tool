108aea50fd7d6000d8a4b6b107e144faa160167b
piyush02 <piyushyadav@live.in>
2024-03-01 02:38:52 +0530

    Update dict_flask.py

--- dict_flask.py
+++ dict_flask.py
@@ -50,4 +50,4 @@ def index():
 if __name__ == '__main__':
     # app.run(debug=True)
     # Run the Flask application on host '0.0.0.0' and port 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('SERVE_PORT', 5000)))
